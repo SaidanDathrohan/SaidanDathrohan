efc39a19764858bb1e9c4f7e2a684121befa7723
SaidanDathrohan <elle13041985@gmail.com>
2023-04-11 16:26:13 +0300

    Test1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [task, setTask] = useState([
+        {learn: 'C++'},
+        {learn: 'JS'},
+        {learn: 'English C2'},
+        {learn: 'Science soft engineering'},
+    ])
+    const [word, setWord] = useState('')
+
+    const getValues = (event: any) => {
+        setWord(event.target.value)
+
+        return true
+    }
+
+    const addTask = () => {
+        if(word.length === 0) {
+            alert("Empty string, write text")
+            return
+        }
+        const arr = [...task, {learn: word}]
+        setTask(arr)
+
+
+        return
+    }
+
+    const deleteTask = (idx: any): any => {
+        const arr = task.filter((item, index) => index !== idx)
+        setTask(arr)
+        return
+    }
+    return (
+        <div className="App"
+             style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',}}>
+            <div>
+                <input onChange={getValues} value={word}/>
+            </div>
+            <div style={{paddingRight: '35px'}}>
+                {task.map((item, idx) => (
+                    <ul key={idx} style={{
+                        listStyleType: "none",
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between'
+                    }}>
+                        <li>{item.learn}</li>
+                        <button onClick={() => deleteTask(idx)}>x</button>
+                    </ul>
+                ))}
+            </div>
+            <div>
+                <button onClick={addTask}>Добавить</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
